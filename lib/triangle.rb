edf30ad25f5a9c5b645b139e60564a6d6d17bff5
Suchitra Naik <susanaik@gmail.com>
2018-05-09 15:57:45 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,14 +6,12 @@ class Triangle
   attr_accessor :side1, :side2, :side3
   
   def initialize(side1, side2, side3)
-    puts "In initialize"
     @side1 = side1
     @side2 = side2
     @side3 = side3
   end 
  
   def kind 
-    
     if [@side1, @side2, @side3].any? { |side| side <= 0 }
       raise TriangleError, "Sides must have positive length" 
     end 
