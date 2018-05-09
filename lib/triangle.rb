1b34ee6b1b29e748a459f5611b4e8371a2ae8564
Suchitra Naik <susanaik@gmail.com>
2018-05-09 15:55:15 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -28,8 +28,11 @@ class Triangle
     
     if(@side1 == @side2) && (@side2 == @side3)
       return ":equilateral"
-    elsif(@side1 == @side2) || (@side2 == @side3) || (@side1 == @side)
-    
+    elsif(@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)
+      return ":isosceles"
+    else 
+      return "scalene"
+    end 
    
   end 
   
