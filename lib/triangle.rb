097e3c11150251db888b1b9808a6f19eb6189631
Suchitra Naik <susanaik@gmail.com>
2018-05-09 15:54:32 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -26,9 +26,9 @@ class Triangle
       raise TriangleError, "Triangle inequality"
     end 
     
-    if(@side1 = @side2) && (@side2 = @side3)
+    if(@side1 == @side2) && (@side2 == @side3)
       return ":equilateral"
-    elsif(@side1 = @side2)
+    elsif(@side1 == @side2) || (@side2 == @side3) || (@side1 == @side)
     
    
   end 
