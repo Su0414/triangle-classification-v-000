50da375b91b400f4bfc00112124020af565cb9bd
Suchitra Naik <susanaik@gmail.com>
2018-05-09 15:26:24 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,12 @@
+require 'pry' 
+
 class Triangle
   # write code here
   
   attr_accessor :length_side1, :length_side2, :length_side3, :array_sides
   
   def initialize(length_side1, length_side2, length_side3)
-    
+    puts "In initialize"
     @length_side1 = length_side1
     @length_side2 = length_side2
     @length_side3 = length_side3
@@ -15,7 +17,10 @@ class Triangle
   end 
   
   def kind 
-    
+    puts "in kind"
+    @array_sides.sort 
+    puts @array_sides 
+     
     if(@length_side1 == @length_side2 == @length_side3 )
       return ":equilateral"
     
