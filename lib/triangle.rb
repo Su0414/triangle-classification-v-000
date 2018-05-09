cf0c60e06df5258f291789865e02890f6c2533ed
Suchitra Naik <susanaik@gmail.com>
2018-05-09 15:24:51 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,13 +1,16 @@
 class Triangle
   # write code here
   
-  attr_accessor :length_side1, :length_side2, :length_side3
+  attr_accessor :length_side1, :length_side2, :length_side3, :array_sides
   
   def initialize(length_side1, length_side2, length_side3)
     
     @length_side1 = length_side1
     @length_side2 = length_side2
     @length_side3 = length_side3
+    
+    @array_sides << length_side1 << length_side2 << length_side3
+    puts @array_sides
     
   end 
   
